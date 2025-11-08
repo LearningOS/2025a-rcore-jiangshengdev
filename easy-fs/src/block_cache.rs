2a814ece595dd5057fccfaf43b55d4ac518b8b18
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-08 14:11:31 +0800

    optimize

--- easy-fs/src/block_cache.rs
+++ easy-fs/src/block_cache.rs
@@ -76,7 +76,7 @@ impl Drop for BlockCache {
     }
 }
 
-const BLOCK_CACHE_SIZE: usize = 16;
+const BLOCK_CACHE_SIZE: usize = 64;
 
 /// BlockCacheManager is a manager for BlockCache.
 pub struct BlockCacheManager {
