60b2c8c1a18507d80068fe0e5bd8828b219c79cd
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-09 10:00:10 +0800

    virtio

--- os/src/drivers/block/virtio_blk.rs
+++ os/src/drivers/block/virtio_blk.rs
@@ -1,18 +1,23 @@
 use super::BlockDevice;
 use crate::mm::{
-    frame_alloc, frame_dealloc, kernel_token, FrameTracker, PageTable, PhysAddr, PhysPageNum,
-    StepByOne, VirtAddr,
+    frame_alloc, kernel_token, FrameTracker, PageTable, PhysAddr as KernelPhysAddr, PhysPageNum,
+    VirtAddr,
 };
 use crate::sync::UPSafeCell;
 use alloc::vec::Vec;
+use core::ptr::NonNull;
 use easy_fs::BLOCK_SZ;
 use lazy_static::*;
-use virtio_drivers::{Hal, VirtIOBlk, VirtIOHeader};
+use virtio_drivers::{
+    device::blk::VirtIOBlk,
+    transport::mmio::{MmioTransport, VirtIOHeader},
+    BufferDirection, Hal, PhysAddr as VirtioPhysAddr,
+};
 
 #[allow(unused)]
 const VIRTIO0: usize = 0x10001000;
 /// VirtIOBlock device driver strcuture for virtio_blk device
-pub struct VirtIOBlock(UPSafeCell<VirtIOBlk<'static, VirtioHal>>);
+pub struct VirtIOBlock(UPSafeCell<VirtIOBlk<VirtioHal, MmioTransport>>);
 
 lazy_static! {
     /// The global io data queue for virtio_blk device
@@ -27,21 +32,17 @@ impl BlockDevice for VirtIOBlock {
     fn read_block(&self, block_id: usize, buf: &mut [u8]) {
         debug_assert_eq!(buf.len(), BLOCK_SZ);
         let mut device = self.0.exclusive_access();
-        for (i, chunk) in buf.chunks_mut(SECTOR_SIZE).enumerate() {
-            device
-                .read_block(block_id * SECTORS_PER_BLOCK + i, chunk)
-                .expect("Error when reading VirtIOBlk");
-        }
+        device
+            .read_blocks(block_id * SECTORS_PER_BLOCK, buf)
+            .expect("Error when reading VirtIOBlk");
     }
     /// Write a block to the virtio_blk device
     fn write_block(&self, block_id: usize, buf: &[u8]) {
         debug_assert_eq!(buf.len(), BLOCK_SZ);
         let mut device = self.0.exclusive_access();
-        for (i, chunk) in buf.chunks(SECTOR_SIZE).enumerate() {
-            device
-                .write_block(block_id * SECTORS_PER_BLOCK + i, chunk)
-                .expect("Error when writing VirtIOBlk");
-        }
+        device
+            .write_blocks(block_id * SECTORS_PER_BLOCK, buf)
+            .expect("Error when writing VirtIOBlk");
     }
 }
 
@@ -56,49 +57,72 @@ impl VirtIOBlock {
     /// Create a new VirtIOBlock driver with VIRTIO0 base_addr for virtio_blk device
     pub fn new() -> Self {
         unsafe {
-            Self(UPSafeCell::new(
-                VirtIOBlk::<VirtioHal>::new(&mut *(VIRTIO0 as *mut VirtIOHeader)).unwrap(),
-            ))
+            let header = NonNull::new(VIRTIO0 as *mut VirtIOHeader).unwrap();
+            let transport =
+                MmioTransport::new(header).expect("failed to create VirtIO MMIO transport");
+            let blk = VirtIOBlk::<VirtioHal, _>::new(transport)
+                .expect("failed to create VirtIO block device");
+            Self(UPSafeCell::new(blk))
         }
     }
 }
 
 pub struct VirtioHal;
 
-impl Hal for VirtioHal {
+unsafe impl Hal for VirtioHal {
     /// allocate memory for virtio_blk device's io data queue
-    fn dma_alloc(pages: usize) -> usize {
+    fn dma_alloc(pages: usize, _direction: BufferDirection) -> (VirtioPhysAddr, NonNull<u8>) {
+        assert!(pages > 0);
         let mut ppn_base = PhysPageNum(0);
+        let mut queue_frames = QUEUE_FRAMES.exclusive_access();
         for i in 0..pages {
-            let frame = frame_alloc().unwrap();
+            let frame = frame_alloc().expect("frame allocation failed for virtio queue");
             if i == 0 {
                 ppn_base = frame.ppn;
+            } else {
+                assert_eq!(frame.ppn.0, ppn_base.0 + i);
             }
-            assert_eq!(frame.ppn.0, ppn_base.0 + i);
-            QUEUE_FRAMES.exclusive_access().push(frame);
+            queue_frames.push(frame);
         }
-        let pa: PhysAddr = ppn_base.into();
-        pa.0
+        drop(queue_frames);
+        let pa: KernelPhysAddr = ppn_base.into();
+        let vaddr = pa.0;
+        let paddr = vaddr as VirtioPhysAddr;
+        let ptr = unsafe { NonNull::new_unchecked(vaddr as *mut u8) };
+        (paddr, ptr)
     }
     /// free memory for virtio_blk device's io data queue
-    fn dma_dealloc(pa: usize, pages: usize) -> i32 {
-        let pa = PhysAddr::from(pa);
-        let mut ppn_base: PhysPageNum = pa.into();
-        for _ in 0..pages {
-            frame_dealloc(ppn_base);
-            ppn_base.step();
-        }
+    unsafe fn dma_dealloc(paddr: VirtioPhysAddr, _vaddr: NonNull<u8>, pages: usize) -> i32 {
+        let base_pa = KernelPhysAddr(paddr);
+        let base_ppn: PhysPageNum = base_pa.into();
+        let mut frames = QUEUE_FRAMES.exclusive_access();
+        let before = frames.len();
+        let start = base_ppn.0;
+        let end = start + pages;
+        frames.retain(|frame| {
+            let ppn = frame.ppn.0;
+            ppn < start || ppn >= end
+        });
+        debug_assert_eq!(before.saturating_sub(frames.len()), pages);
         0
     }
     /// translate physical address to virtual address for virtio_blk device
-    fn phys_to_virt(addr: usize) -> usize {
-        addr
+    unsafe fn mmio_phys_to_virt(paddr: VirtioPhysAddr, _size: usize) -> NonNull<u8> {
+        NonNull::new(paddr as *mut u8).unwrap()
     }
     /// translate virtual address to physical address for virtio_blk device
-    fn virt_to_phys(vaddr: usize) -> usize {
-        PageTable::from_token(kernel_token())
-            .translate_va(VirtAddr::from(vaddr))
-            .unwrap()
-            .0
+    unsafe fn share(buffer: NonNull<[u8]>, _direction: BufferDirection) -> VirtioPhysAddr {
+        let ptr = buffer.cast::<u8>().as_ptr();
+        virt_to_phys(ptr as usize)
     }
+    unsafe fn unshare(_paddr: VirtioPhysAddr, _buffer: NonNull<[u8]>, _direction: BufferDirection) {
+        // Nothing to do, as memory is identity-mapped.
+    }
+}
+
+fn virt_to_phys(vaddr: usize) -> VirtioPhysAddr {
+    PageTable::from_token(kernel_token())
+        .translate_va(VirtAddr::from(vaddr))
+        .expect("virtual address translation failed for virtio share")
+        .0 as VirtioPhysAddr
 }
